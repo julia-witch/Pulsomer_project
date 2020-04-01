6265df8575eadc83b6933f16a57d899972b10327
Julia Gulla <julia1603@gmail.com>
2020-04-01 20:37:05 +0300

    add carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,22 @@
-'use strict';
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        /*adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,*/
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/catalogue/chevron-left.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../img/catalogue/chevron-right.svg"></button>',
 
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+
+        }
+    );
+});
