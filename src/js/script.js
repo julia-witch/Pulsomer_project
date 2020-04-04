566b591728f3de80c3609b3a8b6646975b94a774
Julia Gulla <julia1603@gmail.com>
2020-04-04 19:30:11 +0300

    add final files

--- src/js/script.js
+++ src/js/script.js
@@ -6,17 +6,31 @@ $(document).ready(function(){
         autoplaySpeed: 2000,*/
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/catalogue/chevron-left.svg"></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="../img/catalogue/chevron-right.svg"></button>',
-
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 992,
                 settings: {
                     dots: true,
                     arrows: false
                 }
-            }
-        ]
+            }]
+    });
 
-        }
-    );
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+        $(this)
+            .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+            .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+                $('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+            })
+        });
+    }
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back');
 });
+
