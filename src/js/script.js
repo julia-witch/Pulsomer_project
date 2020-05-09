b133f40b3560b672e7e25322270288e39d12f84d
Julia Gulla <julia1603@gmail.com>
2020-05-09 19:09:30 +0300

    add modal

--- src/js/script.js
+++ src/js/script.js
@@ -37,14 +37,50 @@ $(document).ready(function(){
     $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn('slow');
     });
+
     $('.modal__close').on('click', function () {
         $('.overlay,#consultation, #thanks, #order').fadeOut('slow');
     });
+
     $('.button_mini').each(function (i) {
         $(this).on('click', function () {
             $('#order .modal__descript').text($('.catalogue-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+
+    function validateForms (form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Необходимо ввести не менее {0} символов!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
+
 
